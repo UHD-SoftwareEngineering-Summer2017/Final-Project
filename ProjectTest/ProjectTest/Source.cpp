9de1334ed59ae90e293cd1d2bb9c9b0abcc38b5b
IronBadger51 <scottblick@ymail.com>
2017-06-21 13:25:31 -0500

    Add files via upload

---
+++ ProjectTest/ProjectTest/Source.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "Directory.h"
+
+using namespace std;
+
+int main() {
+	cout << searchDirectory("012345") << endl;
+	addStudent();
+	system("pause");
+}
